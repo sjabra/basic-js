cac8f3e762aeb7d7a3ccab7ac32de51fa635be8b
Alex Valynets <avalynets@fitbit.com>
2020-09-28 22:46:05 +0300

    init: start basic-js tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let reducer = (count, arr) => {
+    return count + arr.filter((item) => item == "^^").length;
+  };
+  return matrix.reduce(reducer, 0);
 };
